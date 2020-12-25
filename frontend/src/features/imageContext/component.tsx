61946755cf1585a7cd9a9f31c0367652a2c4df1e
Lasath Fernando <lafern@microsoft.com>
2020-12-24 22:34:56 -0800

    Add toggle for image context subtitles in frontend

--- frontend/src/features/imageContext/component.tsx
+++ frontend/src/features/imageContext/component.tsx
@@ -1,17 +1,28 @@
+import { mdiSubtitlesOutline } from '@mdi/js';
+import Icon from '@mdi/react';
 import { useTypedSelector } from 'app/hooks';
+import { InlineButton } from 'components/button';
 import { Drawer } from 'components/drawer';
 import { selectBaseUrl } from 'features/backendSelection';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 export const ImageContext: FC<{ episodeId: string; time: number }> = ({
     episodeId,
     time
 }) => {
     const baseUrl = useTypedSelector(selectBaseUrl);
+    const [includeSubs, setIncludeSubs] = useState(false);
     return (
-        <Drawer summary="Image Context">
+        <Drawer
+            summary="Image Context"
+            extraActions={(iconSize) => (
+                <InlineButton onClick={() => setIncludeSubs(!includeSubs)}>
+                    <Icon path={mdiSubtitlesOutline} size={iconSize} />
+                </InlineButton>
+            )}
+        >
             <img
-                src={`${baseUrl}/api/image/${episodeId}/${time}`}
+                src={`${baseUrl}/api/image/${episodeId}/${time}?includeSubs=${includeSubs}`}
                 alt="Screenshot of video at selected dialog time"
             />
         </Drawer>
