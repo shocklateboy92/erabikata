f607ceae6b7f020cbfa3e4f48c5651eedb560d38
Lasath Fernando <lafern@microsoft.com>
2021-01-09 20:21:06 -0800

    Ditch wordSummary component
    It's roles are now all filled by the selectedWord component.

--- frontend/src/pages/rankedWordsPage.tsx
+++ frontend/src/pages/rankedWordsPage.tsx
@@ -1,11 +1,11 @@
 import { Page } from 'components/page';
 import { RankedWords } from 'features/rankedWords';
-import { WordSummary } from 'features/wordSummary/component';
+import { SelectedWord } from 'features/selectedWord';
 import React, { FC } from 'react';
 
 export const RankedWordsPage: FC = () => {
     return (
-        <Page title="Ranked Words" secondaryChildren={() => <WordSummary />}>
+        <Page title="Ranked Words" secondaryChildren={() => <SelectedWord />}>
             <RankedWords />
         </Page>
     );
