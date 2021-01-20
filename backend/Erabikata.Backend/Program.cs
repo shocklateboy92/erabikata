ee59c606042f915ce6033214e66b24a39af2dfb6
Lasath Fernando <lafern@microsoft.com>
2021-01-19 21:11:43 -0800

    Ditch keyvault

--- backend/Erabikata.Backend/Program.cs
+++ backend/Erabikata.Backend/Program.cs
@@ -2,16 +2,13 @@ using System;
 using System.Threading.Tasks;
 using Erabikata.Backend.Managers;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Erabikata.Backend
 {
-    public class Program
+    public static class Program
     {
         public static async Task Main(string[] args)
         {
@@ -29,27 +26,6 @@ namespace Erabikata.Backend
                     (context, config) =>
                     {
                         config.AddJsonFile($"appsettings.{Environment.MachineName}.json", optional: true);
-
-                        var keyVaultName = config.Build()["KeyVaultName"];
-                        if (string.IsNullOrWhiteSpace(keyVaultName))
-                        {
-                            return;
-                        }
-
-                        Console.WriteLine("Attempting to connect KeyVault " + keyVaultName);
-
-                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                        var keyVaultClient = new KeyVaultClient(
-                            new KeyVaultClient.AuthenticationCallback(
-                                azureServiceTokenProvider.KeyVaultTokenCallback
-                            )
-                        );
-
-                        config.AddAzureKeyVault(
-                            $"https://{keyVaultName}.vault.azure.net/",
-                            keyVaultClient,
-                            new DefaultKeyVaultSecretManager()
-                        );
                     }
                 )
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
