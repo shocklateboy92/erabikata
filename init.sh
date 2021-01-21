6ca2fccb834b3631fbdf0e1bb2fd53789bd5d760
Lasath Fernando <lafern@microsoft.com>
2021-01-20 19:15:16 -0800

    Stop `init.sh` from duplicating anime-subs link

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 if [[ -e "./init.sh" ]]; then
 
     export PATH="$(pwd)/ingestion:${PATH}";
-    ln -sf "$IKNOW_DIR/anime-subs" ./anime-subs
+    [[ -e ./anime-subs ]] || ln -sf "$IKNOW_DIR/anime-subs" ./anime-subs
 
 else
     echo "Error: Must be sourced in $ERAB_DIR";
