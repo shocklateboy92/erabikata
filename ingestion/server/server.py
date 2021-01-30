45c18c2d56829943f38bb0f484deb4dbf5f4f38e
Lasath Fernando <lafern@microsoft.com>
2021-01-30 15:20:24 -0800

    Rename `Analyzer` service to `AnalyzerService`
    So it doesn't clash with the enum that's already in backend.

--- ingestion/server/server.py
+++ ingestion/server/server.py
@@ -1,8 +1,29 @@
+import logging
 from sudachipy import tokenizer, dictionary
+from generated import analyzer_pb2
+from generated import analyzer_pb2_grpc
+
+import grpc
+from concurrent import futures
 
 analyzer = dictionary.Dictionary().create()
 
 res = analyzer.tokenize("国家公務員", tokenizer.Tokenizer.SplitMode.A)
 results = [{"surface": a.surface(), "dictionary": a.dictionary_form()} for a in res]
 
-print(results)
+class AnalyzerServicer(analyzer_pb2_grpc.AnalyzerServiceServicer):
+    def AnalyzeText(self, request, context):
+        return analyzer_pb2.AnalyzedResponse(words=results)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    analyzer_pb2_grpc.add_AnalyzerServiceServicer_to_server(AnalyzerServicer(), server)
+    server.add_insecure_port("[::]:5000")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
