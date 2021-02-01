117ebc6e63520ffaffa7bc02521afbb990f681be
Lasath Fernando <lafern@microsoft.com>
2021-01-31 17:18:57 -0800

    Fix enum value generation for frontend

--- backend/Erabikata.Models/Input/Analyzer.cs
+++ backend/Erabikata.Models/Input/Analyzer.cs
@@ -1,5 +1,9 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace Erabikata.Models.Input
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum Analyzer
     {
         Kuromoji,
