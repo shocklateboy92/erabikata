7d0f2ca6937f08386feab69ff622b397b4242589
Lasath Fernando <devel@lasath.org>
2021-03-14 20:24:04 -0700

    Switch `hass/playFrom` to using Rest API
    At least these can be called in parallel, and (hopefully) won't let
    the client get out of whack when plex/hass are being weird.

--- frontend/src/features/hass/api.ts
+++ frontend/src/features/hass/api.ts
@@ -5,6 +5,7 @@ import {
     ThunkDispatch
 } from '@reduxjs/toolkit';
 import { RootState } from 'app/rootReducer';
+import axios from 'axios';
 import * as hass from 'home-assistant-js-websocket';
 import React, { useContext } from 'react';
 import {
@@ -15,22 +16,26 @@ import {
 import { selectSelectedPlayerId } from './selectors';
 
 const STORAGE_KEY = 'hass_state';
+const hassUrl = 'https://home-assistant.apps.lasath.org';
+
+const getAuth = () =>
+    hass.getAuth({
+        hassUrl,
+        saveTokens: (data) =>
+            window.localStorage.setItem(STORAGE_KEY, JSON.stringify(data)),
+        loadTokens: async () => {
+            const data = window.localStorage.getItem(STORAGE_KEY);
+            if (data) {
+                return JSON.parse(data);
+            }
+        }
+    });
 
 const createConnection = async (
     dispatch: ThunkDispatch<unknown, unknown, AnyAction>
 ) => {
     const connection = await hass.createConnection({
-        auth: await hass.getAuth({
-            hassUrl: 'https://home-assistant.apps.lasath.org',
-            saveTokens: (data) =>
-                window.localStorage.setItem(STORAGE_KEY, JSON.stringify(data)),
-            loadTokens: async () => {
-                const data = window.localStorage.getItem(STORAGE_KEY);
-                if (data) {
-                    return JSON.parse(data);
-                }
-            }
-        })
+        auth: await getAuth()
     });
 
     connection.addEventListener('ready', compose(dispatch, hassSocketReady));
@@ -80,29 +85,32 @@ export const pause = createAsyncThunk(
 );
 
 const DOMAIN = 'media_player';
+const PATH_PREFIX = `${hassUrl}/api/services/${DOMAIN}`;
 export const playFrom = createAsyncThunk(
     'hass/playFrom',
     async (
         args: { context: IHassContext; timeStamp: number },
         { getState, dispatch }
     ) => {
-        if (!args.context.connection) {
-            args.context.connection = await createConnection(dispatch);
-        }
-
+        const auth = await getAuth();
         const state = getState();
         // TODO: try making this automagic by wrapping `createAsyncThunk`
         //       with a function that passes through everything, but sets
         //       the 3rd type argument to `RootState`.
         const entity_id = selectSelectedPlayerId(state as RootState);
 
-        await hass.callService(args.context.connection, DOMAIN, 'media_seek', {
-            entity_id,
-            seek_position: args.timeStamp
-        });
-        await hass.callService(args.context.connection, DOMAIN, 'media_play', {
-            entity_id
-        });
+        await Promise.all([
+            axios.post(
+                PATH_PREFIX + '/media_seek',
+                { entity_id, seek_position: args.timeStamp },
+                { headers: { Authorization: `Bearer ${auth.accessToken}` } }
+            ),
+            axios.post(
+                PATH_PREFIX + '/media_play',
+                { entity_id },
+                { headers: { Authorization: `Bearer ${auth.accessToken}` } }
+            )
+        ]);
     }
 );
 
