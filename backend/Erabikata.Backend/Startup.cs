5ec3655a0f4621163c436adc8afbbe4f36ae8ec3
Lasath Fernando <devel@lasath.org>
2021-05-19 17:32:12 -0700

    Add app insights to backend

--- backend/Erabikata.Backend/Startup.cs
+++ backend/Erabikata.Backend/Startup.cs
@@ -30,6 +30,8 @@ namespace Erabikata.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
+
             services.Configure<SubtitleProcessingSettings>(
                 Configuration.GetSection("SubtitleProcessing")
             );
