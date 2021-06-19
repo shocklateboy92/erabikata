2c03f418d9c75ee03666effa2e39bd1c0dedab69
Lasath Fernando <devel@lasath.org>
2021-06-19 13:05:11 -0700

    Split swagger generation into own task in backend

--- deploy-app.sh
+++ deploy-app.sh
@@ -7,15 +7,15 @@ backend_dir=backend/Erabikata.Backend
 backend_build_dir="$backend_dir/bin/Release"
 www_dir="$backend_build_dir/net5.0/publish/wwwroot"
 frontend_build_dir=frontend/build
+configuration=--configuration=Release
 
 # Clear old crap, in case I haven't done a git clean
 rm -rf $backend_build_dir
 rm -rf $frontend_build_dir
 
-dotnet publish --configuration=Release $backend_dir/*.csproj
+dotnet publish $configuration $backend_dir/*.csproj
 
-MAKEFLAGS="-j13" yarn --cwd frontend install
-MAKEFLAGS="-j13" yarn --cwd frontend rtk
+DOTNET_ARGS="$configuration" MAKEFLAGS="-j13" yarn --cwd frontend install
 yarn --cwd frontend build
 cp -rvf "$frontend_build_dir" "$www_dir"
 
