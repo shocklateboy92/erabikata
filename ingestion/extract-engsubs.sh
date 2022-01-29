2ff31565eda00dab301fec3b31247cb5a6cfc790
Lasath Fernando <devel@lasath.org>
2022-01-28 19:43:35 -0800

    Make engsub extractor fancier
    Hopefully, this is enough to handle multiple languages

--- ingestion/extract-engsubs.sh
+++ ingestion/extract-engsubs.sh
@@ -5,4 +5,4 @@ set -e
 root="$(pwd)";
 cd "$(dirname $BASH_SOURCE)/python";
 
-pipenv run python ./engsub-extractor.py "$root";
+pipenv run python ./engsub-extractor.py "$root" "$@";
--- ingestion/python/engsub-extractor.py
+++ ingestion/python/engsub-extractor.py
@@ -7,11 +7,20 @@ import json
 from typing import List, Tuple
 import ffmpeg
 import pprint
+import argparse
 
 MEDIA_ROOT = "/mnt/net/media"
 TRACKS_FILE_NAME = "include_tracks.txt"
 
-input_root = argv[1] if len(argv) > 1 else "../../anime-subs"
+lang_dir_map = { 'eng': 'english', 'jpn': 'input' }
+
+parser = argparse.ArgumentParser(description="Extract subtitles from show media files")
+parser.add_argument('input_root', type=str, default="../../anime-subs")
+parser.add_argument('--language', '--lang', '-l', default="eng", dest='lang', choices=lang_dir_map.keys())
+parser.add_argument('-i', '--info', action=argparse.BooleanOptionalAction, default=False)
+args = parser.parse_args()
+
+input_root = args.input_root
 show_paths = [
     Path(f) for f in glob(f"{input_root}/**/show-metadata.json", recursive=True)
 ]
@@ -21,7 +30,7 @@ for show in show_paths:
     with show.open() as show_f:
         show_info = json.load(show_f)
 
-        out_dir = show.parent.joinpath("english")
+        out_dir = show.parent.joinpath(lang_dir_map[args.lang])
         if not out_dir.exists():
             out_dir.mkdir()
 
@@ -41,8 +50,15 @@ def do_extract(input: str, output: Path):
 
         info = ffmpeg.probe(input)
         subs = [s for s in info["streams"] if s["codec_type"] == "subtitle"]
+
+        if args.info:
+            print(f'{input}:')
+            for sub in subs:
+                print('\t' + pprint.pformat({k: v for k, v in sub['tags'].items() if k in ['title', 'language']}))
+            return
+
         if len(subs) > 1:
-            subs = [s for s in subs if s["tags"]["language"] == "eng"]
+            subs = [s for s in subs if s["tags"]["language"] == args.lang]
 
         if len(subs) > 1:
             tracks_file = output.with_name(TRACKS_FILE_NAME)
@@ -51,6 +67,8 @@ def do_extract(input: str, output: Path):
                     f"File has more than one subtitle but no `{TRACKS_FILE_NAME}` file:\n{pprint.pformat(subs)}"
                 )
 
+            # Should be able to use one tracks file for all languages because subs are
+            # filtered by language before tracks file.
             include_tracks = [s.strip() for s in tracks_file.read_text().splitlines()]
             subs = [f for f in subs if f["tags"].get("title", None) in include_tracks]
 
