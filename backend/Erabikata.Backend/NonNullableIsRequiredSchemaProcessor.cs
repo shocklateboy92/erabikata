1a2dc83daf6aa660b3707a16504485ee95981b4a
Lasath Fernando <devel@lasath.org>
2022-07-23 18:04:27 -0700

    Fix activity properties not being required in swagger spec

--- backend/Erabikata.Backend/NonNullableIsRequiredSchemaProcessor.cs
+++ backend/Erabikata.Backend/NonNullableIsRequiredSchemaProcessor.cs
@@ -7,12 +7,25 @@ public class NonNullableAreRequiredSchemaProcessor : ISchemaProcessor
     public void Process(SchemaProcessorContext context)
     {
         var schema = context.Schema;
-        foreach (var (key, prop) in schema.ActualProperties)
+        foreach (var (key, prop) in schema.Properties)
         {
             if (prop.IsNullableRaw != true)
             {
                 schema.RequiredProperties.Add(key);
             }
+        }
+
+        foreach (var jsonSchema in schema.AllOf)
+        {
+            Process(
+                new SchemaProcessorContext(
+                    context.Type,
+                    jsonSchema,
+                    context.Resolver,
+                    context.Generator,
+                    context.Settings
+                )
+            );
         }
     }
 }
