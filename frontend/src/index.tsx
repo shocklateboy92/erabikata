8e604bf9069d7707a4cea9aae3a85532c978d955
Lasath Fernando <devel@lasath.org>
2022-07-25 23:29:58 -0700

    Fix to createRoot from ReactDOM.render()
    Hopefully this shouldn't break anything since concurrent features
    are opt-in, and strict mode changes are dev-only.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,14 @@
 import { App } from 'App';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './App.css';
 import './app.scss';
 import './index.css';
 import * as serviceWorker from './serviceWorkerRegistration';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
